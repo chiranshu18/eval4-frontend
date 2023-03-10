293ce6646ad72071d690d6cc9b79e6d85e6de414
chiranshu khandelwal <chiranshumain2gmail.com>
2023-03-10 23:44:54 +0530

    feat: add new entry option

--- src/components/entries/index.jsx
+++ src/components/entries/index.jsx
@@ -1,17 +1,65 @@
 import React from 'react';
 import './entries.css';
 import EntryCard from '../entryCard';
+import { useState } from 'react';
 
 import deleteIcon from '../../assets/icons/delete-icons/icon1.png';
 import searchIcon from '../../assets/icons/search-icons/icon1.png';
 
 const index = () => {
+
+
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+
+  function Popup() {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event) => {
+      setInputValue(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      togglePopup();
+    };
+
+    return (
+      <div className="popup2">
+        <h1>Add Enteries</h1>
+        <form >
+          <input className='input-content-type2' type="text" value={inputValue} onChange={handleInputChange} />
+          <input className='input-content-type2' type="text" value={inputValue} onChange={handleInputChange} />
+          <input className='input-content-type2' type="text" value={inputValue} onChange={handleInputChange} />
+          <input className='input-content-type2' type="text" value={inputValue} onChange={handleInputChange} />
+          <input className='input-content-type2' type="text" value={inputValue} onChange={handleInputChange} />
+          <input className='input-content-type2' type="text" value={inputValue} onChange={handleInputChange} />
+          <input className='input-content-type2' type="text" value={inputValue} onChange={handleInputChange} />
+          <input className='input-content-type2' type="text" value={inputValue} onChange={handleInputChange} />
+          <input className='input-content-type2' type="text" value={inputValue} onChange={handleInputChange} />
+          <input className='input-content-type2' type="text" value={inputValue} onChange={handleInputChange} />
+          <input className='input-content-type2' type="text" value={inputValue} onChange={handleInputChange} />
+          
+        </form>
+        <div className='buttons2'>
+          <button className='create-button2' type="submit" onSubmit={handleSubmit}>create</button>
+          <button className='cancel-button2' type="submit" onSubmit={handleSubmit}>cancel</button>
+        </div>
+      </div>
+    );
+  };
+
+
   return (
     <div className='entries'>
 
-      <div className='entries-title'>
+      <div className='entries-title' onClick={togglePopup}>
         <h1 className='total-entries'>13 Entries Found</h1>
         <h2>Add New Entry</h2>
+        {isOpen && <Popup />}
       </div>
 
       <div className='head-entry-card'>
